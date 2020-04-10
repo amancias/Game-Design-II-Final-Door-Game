358e88cb4ef78f4c8dccc3e40bb0a4c1a8edfd02
amancias <amancias@stedwards.edu>
2020-04-10 17:45:20 -0500

    Added character name lists and fixed up the ui overlay problem

--- Test Project/Assets/Scripts/DialogueManager.cs
+++ Test Project/Assets/Scripts/DialogueManager.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+    //A list of names for the characters
+    public List<string> badNames = new List<string>();
+    public List<string> goodNames = new List<string>();
+
+    void Update()
+    {
+        void PickName()
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                //picks a random name from the bad name list outputs it
+                string randomBadName = badNames[Random.Range(0, badNames.Count)];
+                Debug.Log(randomBadName);
+            }
+            else if (Input.GetKeyDown(KeyCode.G))
+            {
+                //picks a random name from the good name list and outputs it
+                string randomGoodName = goodNames[Random.Range(0, goodNames.Count)];
+                Debug.Log(randomGoodName);
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +34,5 @@ public class DialogueManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
