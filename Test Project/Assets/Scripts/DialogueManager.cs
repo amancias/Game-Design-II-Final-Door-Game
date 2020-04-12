7e458a7a278d8725a4a3ab6b8fa337721638e84a
amancias <amancias@stedwards.edu>
2020-04-11 20:17:02 -0500

    back to the way it was

--- Test Project/Assets/Scripts/DialogueManager.cs
+++ Test Project/Assets/Scripts/DialogueManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+    //A list of names for the characters
+    public List<string> badNames = new List<string>();
+    public List<string> goodNames = new List<string>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            PickBadName();
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            PickGoodName();
+        }
+    }
+
+    void PickBadName()
+    {
+        //picks a random name from the bad name list outputs it
+        string randomBadName = badNames[Random.Range(0, badNames.Count)];
+        Debug.Log(randomBadName);
+    }
+
+    void PickGoodName()
+    {
+        //picks a random name from the good name list and outputs it
+        string randomGoodName = goodNames[Random.Range(0, goodNames.Count)];
+        Debug.Log(randomGoodName);
     }
 }
+
