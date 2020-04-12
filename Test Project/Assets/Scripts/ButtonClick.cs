45fb23088f05777bf013acb6c471731bd39dc307
amancias <amancias@stedwards.edu>
2020-04-11 20:10:48 -0500

    character spwaner and button click

--- Test Project/Assets/Scripts/ButtonClick.cs
+++ Test Project/Assets/Scripts/ButtonClick.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class ButtonClick : MonoBehaviour
 {
-
+    public CountdownTimer Timer;
     public void goAway()
     {
         Debug.Log("Go away");
+        Timer.currentTime = 30f;
     }
 
     public void comeIn()
     {
         Debug.Log("Come in");
+        Timer.currentTime = 30f;
     }
-
 }
 
-    
--- Test Project/Assets/Scripts/CharacterSpawner.cs
+++ Test Project/Assets/Scripts/CharacterSpawner.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CharacterSpawner : MonoBehaviour
 {
+    public GameObject goodCharacter = null;
+    public GameObject badCharacter = null;
+    private bool isGood;
+
+    //Gets a random true or false boolean that will check if the upcoming character will be goor or bad
+    public bool GoodOrBad()
+    {
+
+        if (Random.value >= 0.5)
+        {
+            return isGood = true;
+        }
+        return isGood = false;
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        //calls the random good or bad character function at the beginning of the game
+        GoodOrBad();
+
+        //spawns a good or bad character model 
+        if (isGood == true)
+        {
+            Instantiate(goodCharacter, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(badCharacter, transform.position, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
