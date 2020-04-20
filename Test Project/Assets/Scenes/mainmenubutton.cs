7f3a8d5a76b981234324dd97a37bef2f4c1017f7
sclipper1964 <sclipper1964@users.noreply.github.com>
2020-04-19 19:39:17 -0500

    Update mainmenubutton.cs

--- Test Project/Assets/Scenes/mainmenubutton.cs
+++ Test Project/Assets/Scenes/mainmenubutton.cs
@@ -7,7 +7,7 @@ public class mainmenubutton : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
