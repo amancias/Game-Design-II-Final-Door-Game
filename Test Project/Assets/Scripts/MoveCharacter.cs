5a6ba4176e1810c1bb8e0e508d966000315b9644
amancias <alberto.mancias1999@gmail.com>
2020-04-28 04:09:10 -0500

    scene building pt 1

---
+++ Test Project/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    private bool stop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Move the character forward");
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.back * Time.deltaTime * 5f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
