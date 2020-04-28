8050dd25e4bbf5356184d297ab4db1e5d063bfeb
amancias <amancias@stedwards.edu>
2020-04-28 00:57:15 -0500

    strikes

---
+++ Test Project/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+
+    public GameObject strike1;
+    public GameObject strike2;
+    public GameObject strike3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        strike1.SetActive(false);
+        strike2.SetActive(false);
+        strike3.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
